ce8a5702ac6768f7198ebc6f147d3dbe9132aecd
my-note-book <131521192+my-note-book@users.noreply.github.com>
2023-04-23 11:31:42 +0900

    Add files via upload

---
+++ set-background-color.js
@@ -0,0 +1,13 @@
+var colors = ["white", "#080808"];
+var colorIndex = 0;
+
+function changeColor() {
+var col = document.getElementById("body");
+
+
+if( colorIndex >= colors.length ) {
+    colorIndex = 0;
+    }
+    col.style.backgroundColor = colors[colorIndex];
+    colorIndex ++;
+}
